ec93b61fee2078034f13e07285e7dc13df784318
Kim Pykhov <pykhovk@gmail.com>
2026-09-14 21:41:27 +0300

    adjusted script to get list of jobs for a preselected company

--- backend/app/sources/greenhouse.py
+++ backend/app/sources/greenhouse.py
@@ -1,11 +1,18 @@
+import json
 import requests
-from bs4 import BeautifulSoup
 
-response = requests.get("https://www.greenhouse.com/uk")
+COMPANY_TOKEN = "jetbrains"
+
+# todo token for jetbrains - temporary solution, later to be adjusted for a list
+url = f"https://boards-api.greenhouse.io/v1/boards/{COMPANY_TOKEN}/jobs"
+
+response = requests.get(url)
 
 if response.status_code == 200:
-    soup = BeautifulSoup(response.text, "html.parser")
+    data = response.json()
 
-    page_title = soup.title.text
-    print("page title:", page_title)
+    with open("jetbrains_jobs.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
 
+else:
+    print(f"error, server status-code: {response.status_code}")
