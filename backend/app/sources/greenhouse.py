eba1d9801f8f3a17f0bd0c440827dd39a09dd53c
Kim Pykhov <pykhovk@gmail.com>
2026-09-14 22:21:36 +0300

    created Company object for a next fetch

--- backend/app/sources/greenhouse.py
+++ backend/app/sources/greenhouse.py
@@ -1,6 +1,19 @@
 import json
 import requests
 
+from backend.app.domain.company import Company
+
+company = Company(
+    name="JetBrains",
+    website="https://www.jetbrains.com/",
+    careers_url="https://www.jetbrains.com/",
+    offices=["kau", "vil"],
+    ats_type="greenhouse",
+    ats_identifier="jetbrains",
+    enabled=True
+)
+
+
 COMPANY_TOKEN = "jetbrains"
 
 # todo token for jetbrains - temporary solution, later to be adjusted for a list
@@ -11,6 +24,7 @@ response = requests.get(url)
 if response.status_code == 200:
     data = response.json()
 
+    # todo to remove store on disk. shall return the value
     with open("jetbrains_jobs.json", "w", encoding="utf-8") as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
